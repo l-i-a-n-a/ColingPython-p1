1541a065824e8d6055c7cc421960d97852216170
l-i-a-n-a <lianapazhukova@gmail.com>
2021-11-08 16:42:45 +0300

    solution added

--- A.py
+++ A.py
@@ -1,1 +1,19 @@
+class OneIndexedList:
 
+    def __init__(self, items=None):
+        self.items = items or []
+
+    def __getitem__(self, idx):
+        return self.items[idx-1]
+
+    def __setitem__(self, idx, value):
+        self.items[idx-1] = value
+        return value
+
+a = OneIndexedList([])
+a.items.append(0)
+print(a[1])
+
+b = OneIndexedList([2,3,5,8,9,4])
+b[4] = 17
+print(b[4])
